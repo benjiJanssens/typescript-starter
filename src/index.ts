cb631ab6b1be041df48bfd03c97407479f59bfbd
Benjamin Janssens <benji.janssens@gmail.com>
2023-03-02 22:21:54 +0100

    Updated Prettier config, TSConfig and GitHub Actions

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import config from './config';
 
 console.log(
-    `Hello world. You're currently in a ${config.NODE_ENV} environment.`
+  `Hello world. You're currently in a ${config.NODE_ENV} environment.`
 );
